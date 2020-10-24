fd3a43b90f1b6e8575743712602d9579b7d9368b
Ahmad Melegy <ahmad.melegy@gmail.com>
2020-10-25 00:52:31 +0200

    Add no-op for release

---
+++ library-no-op/src/main/java/com/melegy/redscreenofdeath/RedScreenOfDeath.kt
@@ -0,0 +1,11 @@
+package com.melegy.redscreenofdeath
+
+import android.app.Application
+import android.content.Context
+import kotlin.system.exitProcess
+
+object RedScreenOfDeath {
+    fun init(application: Application) {
+        // no-op
+    }
+}
