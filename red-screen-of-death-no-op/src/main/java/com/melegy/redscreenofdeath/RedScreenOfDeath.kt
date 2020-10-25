5bbb57746c14a80325b0583093ed8e28831c314e
Ahmad Melegy <ahmad.melegy@n26.com>
2020-10-25 18:54:24 +0100

    Add support for java

--- red-screen-of-death-no-op/src/main/java/com/melegy/redscreenofdeath/RedScreenOfDeath.kt
+++ red-screen-of-death-no-op/src/main/java/com/melegy/redscreenofdeath/RedScreenOfDeath.kt
@@ -1,10 +1,9 @@
 package com.melegy.redscreenofdeath
 
 import android.app.Application
-import android.content.Context
-import kotlin.system.exitProcess
 
 object RedScreenOfDeath {
+    @JvmStatic
     fun init(application: Application) {
         // no-op
     }
