2898bc971d456049848b6af404bf58ebdbfbfab6
Ahmad Melegy <ahmad.melegy@n26.com>
2020-10-28 08:59:36 +0100

    Move UI code to xml file

--- red-screen-of-death/src/main/java/com/melegy/redscreenofdeath/RedScreenOfDeathActivity.kt
+++ red-screen-of-death/src/main/java/com/melegy/redscreenofdeath/RedScreenOfDeathActivity.kt
@@ -1,89 +1,56 @@
 package com.melegy.redscreenofdeath
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.Intent.*
-import android.graphics.Color
-import android.graphics.Typeface
 import android.os.Build
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
-import android.util.TypedValue
-import android.view.Window
 import android.view.WindowManager
-import android.widget.LinearLayout
-import android.widget.LinearLayout.LayoutParams
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.content.ContextCompat
+import com.melegy.redscreenofdeath.databinding.ActivityRedScreenOfDeathBinding
 
 @SuppressLint("SetTextI18n")
 internal class RedScreenOfDeathActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityRedScreenOfDeathBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityRedScreenOfDeathBinding.inflate(layoutInflater)
 
         supportActionBar?.hide()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            window.statusBarColor = red
-            window.navigationBarColor = red
+            window.statusBarColor = ContextCompat.getColor(this, R.color.red)
+            window.navigationBarColor = ContextCompat.getColor(this, R.color.red)
         }
+        setContentView(binding.root)
 
         val threadName = requireNotNull(intent.getStringExtra(EXTRA_THREAD))
         val throwable = intent.getSerializableExtra(EXTRA_THROWABLE) as Throwable
 
-        LinearLayout(this).apply {
-            orientation = LinearLayout.VERTICAL
-            val padding = convertDpToPx(16f)
-            setPadding(padding, padding, padding, padding)
-            setBackgroundColor(red)
+        renderException(threadName, throwable)
+        logException(throwable)
+    }
 
-            val params = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-            addView(TextView(this@RedScreenOfDeathActivity).apply {
-                setDefaultTextViewStyle()
-                setTypeface(typeface, Typeface.BOLD)
-                textSize = 18f
-                text = "App crashed in $threadName thread"
-            }, params)
+    private fun renderException(threadName: String, throwable: Throwable) {
+        binding.textThreadName.text = "App crashed in $threadName thread"
+        binding.textException.text = throwable.javaClass.simpleName
+        binding.textStackTrace.text = throwable.stackTraceToString()
+        binding.textStackTrace.movementMethod = ScrollingMovementMethod()
+    }
 
-            addView(TextView(this@RedScreenOfDeathActivity).apply {
-                setDefaultTextViewStyle()
-                setTypeface(typeface, Typeface.BOLD)
-                textSize = 14f
-                setPadding(0, convertDpToPx(16f), 0, 0)
-                text = throwable.javaClass.simpleName
-            }, params)
-
-            addView(TextView(this@RedScreenOfDeathActivity).apply {
-                setDefaultTextViewStyle()
-                textSize = 14f
-                isVerticalScrollBarEnabled = true
-                movementMethod = ScrollingMovementMethod()
-                setPadding(0, convertDpToPx(8f), 0, 0)
-                text = throwable.stackTraceToString()
-            }, params)
-        }.also { setContentView(it) }
-
-        Logger.logger.e("═══════════ Exception caught by RedScreenOfDeath library ═══════════")
+    private fun logException(throwable: Throwable) {
+        Logger.logger.e("═══════════ Exception caught by Red Screen Of Death library ═══════════")
         Logger.logger.e(throwable.javaClass.simpleName, throwable)
         Logger.logger.e("════════════════════════════════════════════════════════════════════")
     }
 
-    private fun TextView.setDefaultTextViewStyle() = also {
-        typeface = Typeface.MONOSPACE
-        setTextColor(Color.parseColor("#FFFF66"))
-    }
-
-    private fun Activity.convertDpToPx(dp: Float) =
-        TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.displayMetrics).toInt()
-
     companion object {
-        private val red = Color.parseColor("#880000")
-
         private const val EXTRA_THREAD = "com.melegy.redscreenofdeath.EXTRA_THREAD"
         private const val EXTRA_THROWABLE = "com.melegy.redscreenofdeath.EXTRA_THROWABLE"
 
